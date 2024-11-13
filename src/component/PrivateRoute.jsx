ff24d24701375533db4781a830a5a43c61befc3c
PYSchycho <virdimehak44@gmail.com>
2024-11-13 10:27:51 +0530

    PrivateRoute.jsx

--- src/component/PrivateRoute.jsx
+++ src/component/PrivateRoute.jsx
@@ -1,10 +1,7 @@
 import { Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-
-
 const PrivateRoute = ({ Component }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  
   useEffect(() => {
     const accessToken =localStorage.getItem('accessToken');
     if (accessToken) {
@@ -13,8 +10,6 @@ const PrivateRoute = ({ Component }) => {
         setIsAuthenticated(false);
       }
   },[]);
-    
   return isAuthenticated ? <Component /> : <Navigate to="/login" />;
 };
-
 export default PrivateRoute;
