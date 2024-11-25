bf53c006f9a74ea905006c7d251a38c160b53723
Mehak <virdimehak44@gmail.com>
2024-11-25 17:33:38 +0530

    cart page with its functionality, Address page , Payment page

---
+++ ecommerce-app/ecommerce-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './index.css';
+import Main from './component/Main';
+
+const App = () => {
+  return (
+    <Main/>
+    
+  );
+}
+
+export default App;
